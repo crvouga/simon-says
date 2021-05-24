3351af015214ceb06761e221b2fe4a71ae2efef0
Christopher Vouga <crvouga@gmail.com>
2021-05-24 13:40:00 -0700

    removed ugly buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
-import { Box, Button, Container, makeStyles } from "@material-ui/core";
-import CodeIcon from "@material-ui/icons/Code";
+import { Box, Container, makeStyles } from "@material-ui/core";
 import React from "react";
 import { Game } from "./Game";
 import { LoadingBackdrop } from "./LoadingBackdrop";
-import LinkIcon from "@material-ui/icons/Link";
+
 const useStyles = makeStyles((theme) => ({
   buttons: {
     width: "100%",
@@ -16,55 +15,18 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const PERSONAL_URL = `https://chrisvouga.dev/`;
-
-const AttributionButton = () => {
-  const classes = useStyles();
-
-  return (
-    <Button
-      className={classes.button}
-      variant="outlined"
-      href={PERSONAL_URL}
-      startIcon={<LinkIcon />}
-    >
-      Chris Vouga
-    </Button>
-  );
-};
-
-const SOURCE_CODE_URL = "https://github.com/crvouga/simon-says";
-
-const SourceCodeButton = (props) => {
-  const classes = useStyles();
-
-  return (
-    <Button
-      className={classes.button}
-      variant="outlined"
-      href={SOURCE_CODE_URL}
-      startIcon={<CodeIcon />}
-    >
-      Source Code
-    </Button>
-  );
-};
-
 export const App = () => {
   const classes = useStyles();
   return (
     <React.Fragment>
-      <div className={classes.buttons}>
-        <AttributionButton />
-        <SourceCodeButton />
-      </div>
-
       <Box paddingY={4}>
         <Container maxWidth="xs">
           <Game />
         </Container>
       </Box>
+
       <LoadingBackdrop />
+
       <div className={classes.footer}></div>
     </React.Fragment>
   );
